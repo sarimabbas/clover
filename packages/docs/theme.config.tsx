bd8fa5993b91f9cdfc8a37a8ab412062b5d9e77d
Sarim Abbas <sarim.stays@gmail.com>
2023-05-10 12:09:37 -0700

    wio

--- packages/docs/theme.config.tsx
+++ packages/docs/theme.config.tsx
@@ -1,7 +1,31 @@
-export default {
-  logo: <span>Clover Documentation</span>,
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Clover ☘️</span>,
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Clover",
+    };
+  },
+  head: (
+    <>
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Clover" />
+      <meta
+        property="og:description"
+        content="Server routes augmented with Zod and OpenAPI"
+      />
+      <meta property="og:image" content="https://clover.vercel.app/cover.png" />
+    </>
+  ),
+  footer: {
+    text: <p>MIT {new Date().getFullYear()} © Clover.</p>,
+  },
   project: {
     link: "https://github.com/sarimabbas/clover",
   },
-  // ...
+  docsRepositoryBase:
+    "https://github.com/sarimabbas/clover/tree/main/packages/docs",
 };
+
+export default config;
